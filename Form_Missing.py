23bcc227662644fe1e141dcbe728b9c5f96a085f
Aldahir Alonso <aldahir0603@hotmail.com>
2026-03-06 16:31:05 -0600

    Segundo avance: Datos de formulario.

--- Form_Missing.py
+++ Form_Missing.py
@@ -4,11 +4,35 @@ name = _items[0]["json"]["Nombre"]
 email = _items[0]["json"]["Email"]
 message = _items[0]["json"]["Mensaje"]
 
+clean_name = " ".join(name.split())
+clean_email = email.strip().lower()
+text = message.lower()
+
+score = 0
+
+if "urgente" in text or "lo antes posible" in text:
+  score += 50
+
+if "asesoria" in text or "curso" in text:
+  score += 30
+
+if len(text) > 120:
+  score +=20
+
+if score >= 70:
+  level = "caliente"
+elif score >= 40:
+  level = "medio"
+else:
+  level = "frio"
+
 results.append({
     "json": {
-      "name": name,
-      "email": email,
-      "message": message
+      "name": clean_name,
+      "email": clean_email,
+      "message": message,
+      "score": score,
+      "level": level
     }
 })
 
